7392b8ff677e52f77afcc26f40a76ee3a4614f5a
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-01-22 12:13:41 -0300

    Projeto E-Commerce Djando 03.git{views.py;urls.py}

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 from django.contrib import admin
-from .views import home_page
-
+from .views import home_page, about_page, contact_page
 urlpatterns = [
         path('', home_page),
+        path('about/', about_page),
+	path('contact/', contact_page),
         path('admin/', admin.site.urls),
-]
+        ]
