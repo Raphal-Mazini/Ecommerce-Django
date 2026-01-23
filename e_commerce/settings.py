ca2d198b656ee5675bcbbd78b2bd6abc874e6323
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-01-23 12:20:16 -0300

    Projeto E-Commerce Django -- Atualizacao: e_commerce/settings.py -- Static

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static_local")
+    os.path.join(BASE_DIR, "static_local")
 ]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
