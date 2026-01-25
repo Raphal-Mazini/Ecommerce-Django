c003d22b43bab7c76b842670a5eb0c748c93faab
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-01-25 18:14:13 -0300

    Projeto e-Commerce Django -- create and update:e_commerce/urls.py &  products/views.py & template/products/detail.html & template/products/featured-detail.html

--- products/views.py
+++ products/views.py
@@ -5,17 +5,22 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+    
+    def get_queryset(self, *args, **kwargs):
+        return Product.objects.featured()
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
 #Class Based View
 class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-
-    # def get_context_data(self, *args, **kwargs):
-    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
-    #     print(context)
-    #     return context
-
+    
 
 #Function Based View
 def product_list_view(request):
