8dbfb1d8e831a6f523b439e5c7ac9245869a99ca
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-01-30 18:47:59 -0300

    Proejto E-Commerce Django -- update: e_commerce/urls.py & products/urls.py & template/base/navbar.html

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,33 +4,23 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from products.views import (
-        ProductListView,
-        product_list_view,
-        ProductDetailView, 
-        product_detail_view,
-        ProductDetailSlugView,
-        ProductFeaturedListView,
-        ProductFeaturedDetailView)
-
-from .views import (
-        home_page, 
-        about_page, 
-        contact_page, 
-        login_page, 
-        register_page)
-
+from .views import (home_page, 
+                    about_page, 
+                    contact_page, 
+                    login_page, 
+                    register_page
+)
 
 urlpatterns = [
-	path('', home_page),
-	path('about/', about_page),
-	path('contact/', contact_page),
-        path('login/', login_page),
-        path('register/', register_page),
-        path('products/', include("products.urls", namespace="products")),
-        path('admin/', admin.site.urls)
-        ]
+    path('', home_page, name='home'),
+    path('about/', about_page, name='about'),
+    path('contact/', contact_page, name='contact'),
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    path('products/', include("products.urls", namespace="products")),
+    path('admin/', admin.site.urls),
+]
 
 if settings.DEBUG:
-        urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-        urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
