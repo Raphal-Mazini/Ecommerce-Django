d3b5bb508f37f0a93641929b797533a48ed714b6
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-02 16:40:19 -0300

    Projeto E-Commerce Django -- create and update -- app cart

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,24 +4,29 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import (
-    home_page,
-    about_page,
-    contact_page, 
-    login_page, 
-    logout_page,
-    register_page
+from carts.views import cart_home
+
+
+from .views import (home_page, 
+                    about_page, 
+                    contact_page, 
+                    login_page, 
+                    logout_page,
+                    register_page
 )
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('about/', about_page, name='about'),
     path('contact/', contact_page, name='contact'),
+    path('cart/', cart_home, name='cart'),
     path('login/', login_page, name='login'),
     path('logout/', logout_page, name='logout'),
     path('register/', register_page, name='register'),
-    path('products/', include("products.urls", namespace="products")),
     path('search/', include("search.urls", namespace="search")),
+    
+    path('products/', include("products.urls", namespace="products")),
+    
     path('admin/', admin.site.urls),
 ]
 
