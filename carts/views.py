7ed1738db8c901b7636333ba0b4164ee0143ed90
Raphael_Mazini <raphaelmazini37@gmail.com>
2026-02-07 13:23:24 -0300

    create: and update: accounts & billing -- 07/02/2026 -- 13:23

--- carts/views.py
+++ carts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+
+from accounts.forms import LoginForm
+from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
 from .models import Cart
@@ -32,8 +35,17 @@ def checkout_home(request):
     #ou se o carrinho já existir mas não tiver nada dentro
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")
-
     #aqui a order associada ao carrinho
     else:
         order_obj, new_order_obj = Order.objects.get_or_create(cart = cart_obj)
-    return render(request, "carts/checkout.html", {"object": order_obj})
+    user = request.user
+    billing_profile = None
+    login_form = LoginForm()
+    if user.is_authenticated:
+        billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(user = user, email = user.email)
+    context = {
+        "object": order_obj,
+        "billing_profile": billing_profile,
+        "login_form ": login_form 
+    }
+    return render(request, "carts/checkout.html", context)
